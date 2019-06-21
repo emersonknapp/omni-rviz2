025f0e561ebca44a30d3a141d149cf0f718c993e
Emerson Knapp <eknapp@amazon.com>
2019-06-21 00:49:33 -0700

    Use React

--- rviz_web/src/index.js
+++ rviz_web/src/index.js
@@ -1,1 +1,9 @@
-console.log("Hellooooopoop!")
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Wow React';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
--- rviz_web/webpack.config.js
+++ rviz_web/webpack.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
